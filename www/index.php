97f59034406a118367ea5d6c6774f72e1838d7b9
mcasl <mcasl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-16 14:59:33 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/amore@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> <a file="unitTestResults.html">UNIT TEST RESULTS, (powered by the RUnit package)</a>. </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
