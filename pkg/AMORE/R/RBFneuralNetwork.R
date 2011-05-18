cbbebba0bfffaccb06645f4ab1e982d359afc944
mcasl <mcasl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-18 18:22:04 +0000

    listLayer, listMLPlayer and listRBFlayer, with unit tests, added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/amore@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/AMORE/R/RBFneuralNetwork.R
@@ -0,0 +1,10 @@
+# TODO: Add comment
+# 
+# Author: mcasl
+###############################################################################
+
+gRBFneuralNetwork <- setRefClass("RBFneuralNetwork",
+		contains="neuralNetwork",
+		methods= list()
+)
+
--- pkg/AMORE/R/listRBFlayer.R
+++ pkg/AMORE/R/listRBFlayer.R
@@ -3,5 +3,20 @@
 # Author: mcasl
 ###############################################################################
 
+gListRBFLayer <- setRefClass("listRBFLayer",
+		contains="listLayer",
+		methods = list(	
+				
+				validate = function(...){
+					lapply(getLdata(), function(x){ 
+								if(!is(x,"rbfLayer")) {stop("[listRBFlayer Validate]: Error, layer does not belogn to the rbfLayer class.")} else {} 
+							} )
+					callSuper(...)
+					return(TRUE)
+				}
+		
+		)
+)
+
 
 
