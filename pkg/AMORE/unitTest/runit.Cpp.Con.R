8fb3859f74858a295f735bbf5f66ef5689004586
mcasl <mcasl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-24 22:20:57 +0000

    Con C++ class unit tests completed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/amore@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/AMORE/unitTest/runit.Cpp.Con.R
+++ pkg/AMORE/unitTest/runit.Cpp.Con.R
@@ -1,41 +1,122 @@
-# Unit Tests for the Con class methods
+# Unit Tests for the Con C++ class methods
 # 
 # Author: mcasl
 ###############################################################################
 
 
-test.Con.Cpp.class <- function() {	
+test.Con.Cpp.setAndgetFromNeuron <- function() {	
+	funCode <- '
+			Con micon;
+			Neuron nn;
+			Neuron * ptnn;
+			nn.setId(1);
+			micon.setFromNeuron(&nn);
+			
+			ptnn = micon.getFromNeuron();
+			int result= ptnn->getId();
+			return wrap(result);
+			'
 	
+	incCode <- paste( readLines( "pkg/AMORE/src/Con.cpp"), collapse = "\n" )
+	fx <- cxxfunction( signature(), body=funCode , include = incCode, plugin = "Rcpp", verbose=FALSE )
+	result <- fx()
+	checkEquals(result, 1)
+}
 
-funCode <- '
-int getFromIdResult;
-double getWeightResult;
-bool showResult;
-Con micon;
-Neuron nn;
-nn.setId(1);
-micon.setFromNeuron(&nn);
-micon.setWeight(1.4);
 
-showResult=micon.show();
-getWeightResult= micon.getWeight();
-getFromIdResult= micon.getFromId();
+test.Con.Cpp.getFromId<- function() {	
+	funCode <- '
+			Con micon;
+			Neuron nn;
+			nn.setId(16);
+			micon.setFromNeuron(&nn);
+			
+			int result= micon.getFromId();
+			return wrap(result);
+			'
+	
+	incCode <- paste( readLines( "pkg/AMORE/src/Con.cpp"), collapse = "\n" )
+	fx <- cxxfunction( signature(), body=funCode , include = incCode, plugin = "Rcpp", verbose=FALSE )
+	result <- fx()
+	checkEquals(result, 16)
+}
 
-return Rcpp::List::create(	Rcpp::Named("show")			= showResult,
-							Rcpp::Named("getFromId") 	= getFromIdResult,
-							Rcpp::Named("getWeight")	= getWeightResult);
-'
 
-incCode <- paste( readLines( "pkg/AMORE/src/Con.cpp"), collapse = "\n" )
-fx <- cxxfunction( signature(), body=funCode , include = incCode, plugin = "Rcpp", verbose=FALSE )
+test.Con.Cpp.setAndgetWeight <- function() {	
+	funCode <- '
+			Con micon;
+			Neuron nn;
+			nn.setId(16);
+			micon.setFromNeuron(&nn);
+			micon.setWeight(12.4);
+			double result1= micon.getWeight();
+			micon.setWeight(2.2);
+			double result2= micon.getWeight();
+			return Rcpp::List::create(	Rcpp::Named("R1")	= result1,
+										Rcpp::Named("R2") 	= result2);
+			'
+	incCode <- paste( readLines( "pkg/AMORE/src/Con.cpp"), collapse = "\n" )
+	fx <- cxxfunction( signature(), body=funCode , include = incCode, plugin = "Rcpp", verbose=FALSE )
+	result <- fx()
+	checkEquals(result$R1, 12.4)
+	checkEquals(result$R2, 2.2)
+}
 
-result <- fx()
 
-checkEquals(result$show, TRUE)
-checkEquals(result$getFromId, 1)
-checkEquals(result$getWeight, 1.4)
+test.Con.Cpp.show <- function() {	
+	funCode <- '
+			Con micon;
+			Neuron nn;
+			nn.setId(16);
+			micon.setFromNeuron(&nn);
+			micon.setWeight(12.4);
+			bool result= micon.show();
+			return wrap(result);
+			'
+	incCode <- paste( readLines( "pkg/AMORE/src/Con.cpp"), collapse = "\n" )
+	fx <- cxxfunction( signature(), body=funCode , include = incCode, plugin = "Rcpp", verbose=FALSE )
+	result <- fx()
+	checkTrue(result)
 
 }
+
+
+
+test.Con.Cpp.validate.weight <- function() {	
+	funCode <- '
+			Con micon;
+			Neuron nn;
+			nn.setId(16);
+			micon.setFromNeuron(&nn);
+			micon.setWeight(1.4/0);
+			micon.validate();
+			return wrap(true);
+			'
+	incCode <- paste( readLines( "pkg/AMORE/src/Con.cpp"), collapse = "\n" )
+	fx <- cxxfunction( signature(), body=funCode , include = incCode, plugin = "Rcpp", verbose=FALSE )
+	checkException(result <- fx(), silent=TRUE)
+}
+
+
+test.Con.Cpp.validate.from <- function() {
+	
+	funCode <- '
+			Con micon;
+			Neuron nn;
+			nn.setId(NA_INTEGER);
+			micon.setFromNeuron(&nn);
+			micon.setWeight(1.4);
+			micon.validate();
+			return wrap(micon.getFromId());
+			'
+	incCode <- paste( readLines( "pkg/AMORE/src/Con.cpp"), collapse = "\n" )
+	fx <- cxxfunction( signature(), body=funCode , include = incCode, plugin = "Rcpp", verbose=FALSE )
+	checkException(result <- fx(), silent=TRUE)
+
+
+	
+}
+
 
 
 
