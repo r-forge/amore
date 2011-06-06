2842ffff805654aa46d88a8efecbed4519cdb097
mcasl <mcasl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-06 20:23:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/amore@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/AMORE/src/otro-VecCon.h
@@ -0,0 +1,38 @@
+/*
+ *  VecCon.h
+ *
+ *  Created on: 26/05/2011
+ *      Author: mcasl
+ */
+
+#ifndef VECCON_H_
+#define VECCON_H_
+
+
+//!  A vector of connections
+/*!
+ *   The VecCon class provides a simple class for a vector of connections. It's named after the R equivalent Reference Class.
+ */
+
+class VecCon: public Container<Con> {
+
+public:
+
+									int	numOfCons();
+					std::vector<int>	getId();
+								bool	buildAndAppend	( std::vector<NeuronPtr> vFrom , std::vector<double> vWeight );
+					std::vector<double>	getWeight	  	( );
+					std::vector<double>	getWeight	  	( std::vector<int> vFrom );
+		std::vector<NeuronPtr>	getFrom 	( );
+								bool	setWeight		( std::vector<double> vWeight);
+								bool	setWeight		( std::vector<double> vWeight, std::vector<int> vFrom);
+								bool	setFrom	( std::vector<NeuronPtr> vFrom);
+								void	erase 			( std::vector<int> vFrom );
+							VecConPtr	select			( std::vector<int> vFrom );
+								bool	validate		( );
+
+};
+
+#endif /* VECCON_H_ */
+
+
---
+++ pkg/AMORE/src/otro-VecNeuron.h
@@ -0,0 +1,38 @@
+/*
+ *  vecNeuron.h
+ *
+ *  Created on: 03/06/2011
+ *  Author: mcasl
+ *
+ */
+
+#ifndef VECNEURON_H_
+#define VECNEURON_H_
+
+
+//!  A vector of neurons
+/*!
+ *   The vecNeuron class provides a simple class for a vector of neurons. It's named after the R equivalent Reference Class.
+ */
+
+class vecNeuron: public Container<Neuron> {
+
+public:
+
+//									int	numOfNeurons();
+//					std::vector<int>	getId();
+//					std::vector<double>	getWeight	  	( );
+//					std::vector<double>	getWeight	  	( std::vector<int> FROM );
+//		std::vector<NeuronPtr>	getFrom 	( );
+//								void	setWeight		( std::vector<double> value);
+//		À?						void	setWeight		( std::vector<double> value, std::vector<int> FROM);
+//								void	setFrom	( std::vector<NeuronPtr> FROM);
+//								void 	erase 			( std::vector<int> FROM );
+//								VecCon	select			( std::vector<int> FROM );
+//								bool	validate		( );
+
+};
+
+#endif /* VECNEURON_H_ */
+
+
