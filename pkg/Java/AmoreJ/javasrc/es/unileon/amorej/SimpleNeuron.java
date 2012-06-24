aed9e720631b3d98995504d5560cc677afb18469
mcasl (Dr. ) <manuel.castejon@gmail.com>
2012-06-24 20:14:48 +0000

    Connection + unit tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/amore@187 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Java/AmoreJ/javasrc/es/unileon/amorej/SimpleNeuron.java
+++ pkg/Java/AmoreJ/javasrc/es/unileon/amorej/SimpleNeuron.java
@@ -4,6 +4,21 @@ public class SimpleNeuron implements Neuron {
 
 	private String id;
 
+	
+//	  NeuralNetwork neuralNetwork;
+//	  PredictBehavior predictBehavior;
+//	  ActivationFunction activationFunction;
+//	  NeuronTrainBehavior neuronTrainBehavior;
+	//  ConContainer nCons;
+	  double inducedLocalField;
+	  double output;
+	  double outputDerivative;
+	  double target;
+	 // ConIteratorPtr conIterator;
+
+	
+	
+	
 	public SimpleNeuron(String newId) {
 		this.id = newId;
 	}
@@ -15,6 +30,276 @@ public class SimpleNeuron implements Neuron {
 	public void setId(String newId) {
 		this.id = newId;
 	}
+
+	
+//
+//	SimpleNeuron::SimpleNeuron(NeuralFactory* ptNeuralFactory) :
+//	  Neuron(ptNeuralFactory)
+//	{
+//	}
+
+//	double
+//	SimpleNeuron::getInducedLocalField()
+//	{
+//	  return d_inducedLocalField;
+//	}
+
+	
+//	void
+//	SimpleNeuron::setInducedLocalField(double inducedLocalField)
+//	{
+//	  d_inducedLocalField = inducedLocalField;
+//	}
+//
+	
+	
+//	inline
+//	double
+//	SimpleNeuron::getOutput()
+//	{
+//	  return d_output;
+//	}
+//
+	
+	
+//	void
+//	SimpleNeuron::setOutput(double output)
+//	{
+//	  d_output = output;
+//	}
+//
+//
+//
+//	double
+//	SimpleNeuron::getTarget()
+//	{
+//	  return d_target;
+//	}
+//
+//	void
+//	SimpleNeuron::setTarget(double target)
+//	{
+//	  d_target = target;
+//	}
+//
+//
+//	double
+//	SimpleNeuron::getOutputDerivative()
+//	{
+//	  return d_outputDerivative ;
+//	}
+//
+//
+//
+//	void
+//	SimpleNeuron::setOutputDerivative(double outputDerivative)
+//	{
+//	  d_outputDerivative = outputDerivative;
+//	}
+//
+//
+//	Handler
+//	SimpleNeuron::getId()
+//	{
+//	  return d_Id;
+//	}
+//
+//
+//	void
+//	SimpleNeuron::setId(Handler Id)
+//	{
+//	  d_Id = Id;
+//	}
+//
+//	ConIteratorPtr&
+//	SimpleNeuron::getConIterator()
+//	{
+//	  return d_conIterator;
+//	}
+//
+//	void
+//	SimpleNeuron::addCon(ConPtr& conPtr)
+//	{
+//	  d_nCons->push_back(conPtr);
+//	}
+//
+//
+//	void
+//	SimpleNeuron::setActivationFunction(ActivationFunctionPtr& activationFunctionPtr)
+//	{
+//	  d_activationFunction = activationFunctionPtr;
+//	}
+//
+//
+//	void
+//	SimpleNeuron::setPredictBehavior(PredictBehaviorPtr& predictBehaviorPtr)
+//	{
+//	  d_predictBehavior = predictBehaviorPtr;
+//	}
+//
+//
+//
+//	void
+//	SimpleNeuron::setNeuronTrainBehavior(NeuronTrainBehaviorPtr& neuronTrainBehaviorPtr)
+//	{
+//	  d_neuronTrainBehavior = neuronTrainBehaviorPtr;
+//	}
+//
+//
+//
+//	std::string
+//	SimpleNeuron::getNeuronTrainBehaviorName()
+//	{
+//	  return d_neuronTrainBehavior->getName();
+//	}
+//
+//
+//	NeuralNetwork*
+//	SimpleNeuron::getNeuralNetwork( )
+//	{
+//	  return d_neuralNetwork;
+//	}
+//
+//
+//	void
+//	SimpleNeuron::setNeuralNetwork( NeuralNetworkPtr& neuralNetworkPtr)
+//	{
+//	  d_neuralNetwork = neuralNetworkPtr.get();
+//	}
+//
+//
+//
+//	double
+//	SimpleNeuron::useActivationFunctionf0()
+//	{
+//	  return d_activationFunction->f0();
+//	}
+//
+//
+//	double
+//	SimpleNeuron::useActivationFunctionf1()
+//	{
+//	  return d_activationFunction->f1();
+//	}
+//
+//
+//
+//	void
+//	SimpleNeuron::singlePatternForwardAction()
+//	{
+//	  d_predictBehavior->singlePatternForwardAction();
+//	}
+//
+//
+//	void
+//	SimpleNeuron::singlePatternBackwardAction()
+//	{
+//	  d_neuronTrainBehavior->singlePatternBackwardAction();
+//	}
+//
+//
+//	void
+//	SimpleNeuron::show()
+//	{
+//	  if (d_nCons->size() == 0)
+//	    {
+//
+//	      int id = getId();
+//	      Rprintf("\n\n-----------------------------------");
+//	      if (id == NA_INTEGER)
+//	        {
+//	          Rprintf("\n Id: NA, Invalid neuron Id");
+//	        }
+//	      else
+//	        {
+//	          Rprintf("\n Id: %d", id);
+//	        }
+//	      Rprintf("\n-----------------------------------");
+//	      Rprintf("\n output: %lf", d_output);
+//	      Rprintf("\n-----------------------------------");
+//	      Rprintf("\n Neuron Train Behavior: %s", getNeuronTrainBehaviorName().c_str() );
+//	      Rprintf("\n-----------------------------------");
+//
+//	    }
+//	  else
+//	    {
+//	      int id = getId();
+//	      Rprintf("\n\n-----------------------------------");
+//	      if (id == NA_INTEGER)
+//	        {
+//	          Rprintf("\n Id: NA, Invalid neuron Id");
+//	        }
+//	      else
+//	        {
+//	          Rprintf("\n Id: %d", id);
+//	        }
+//	      Rprintf("\n-----------------------------------");
+//	      d_predictBehavior->show();
+//	      Rprintf("\n output: %lf", d_output);
+//	      Rprintf("\n target: %lf", d_target);
+//	      Rprintf("\n-----------------------------------");
+//	      d_nCons->show();
+//	      Rprintf("\n-----------------------------------");
+//	      Rprintf("\n Neuron Train Behavior: %s", getNeuronTrainBehaviorName().c_str() );
+//	      Rprintf("\n-----------------------------------");
+//	    }
+//	}
+//
+//	bool
+//	SimpleNeuron::validate()
+//	{
+//	  BEGIN_RCPP
+//	  if (getId() == NA_INTEGER ) throw std::range_error("[C++ SimpleNeuron::validate]: Error, Id is NA.");
+//	    d_nCons->validate();
+//	  return (TRUE);
+//	END_RCPP}
+//
+//
+//	int
+//	SimpleNeuron::numberOfConnections()
+//	{
+//	  return static_cast<int>(d_nCons->size());
+//	}
+//
+//
+//
+//	double
+//	SimpleNeuron::costFunctionf0(double output, double target)
+//	{
+//	  return d_neuralNetwork->costFunctionf0( output, target );
+//	}
+//
+//
+//	double
+//	SimpleNeuron::costFunctionf1(double output, double target)
+//	{
+//	  return d_neuralNetwork->costFunctionf1( output, target );
+//	}
+//
+//
+//
+//	void
+//	SimpleNeuron::addToBias(double value)
+//	{
+//	  d_predictBehavior->addToBias(value);
+//	}
+//
+//
+//	void
+//	SimpleNeuron::addToDelta(double value)
+//	{
+//	  d_neuronTrainBehavior->addToDelta(value);
+//	}
+//
+//	void
+//	SimpleNeuron::setLearningRate(double learningRate)
+//	{
+//	  d_neuronTrainBehavior->setLearningRate(learningRate);
+//	}
+//
+//
+//
+//
 
 
 }
--- pkg/Java/AmoreJ/test/es/unileon/amorej/ConTests.java
+++ pkg/Java/AmoreJ/test/es/unileon/amorej/ConTests.java
@@ -12,11 +12,96 @@ public class ConTests {
 	}
 
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void testConstructor() throws AmoreJException {
+		Neuron neuron = new SimpleNeuron("1");
+		Connection connection = new Connection(neuron, 4.5);
+		connection.show();
+		connection.validate();
+		assertTrue(connection.getId().equals("1"));
+		assertEquals(connection.getWeight(), 4.5, 1e-10);
+	}
+
+	@Test
+	public void testGetNeuron() {
+		Neuron neuron = new SimpleNeuron("12");
+		Connection connection = new Connection(neuron, 9.8);
+		assertTrue(connection.getNextNeuron().getId().equals("12"));
+	}
+
+	@Test
+	public void testSetNeuron() {
+		Neuron neuron1 = new SimpleNeuron("12");
+		Connection connection = new Connection(neuron1, 9.7);
+		assertTrue(connection.getNextNeuron().getId().equals("12"));
+		connection.show();
+		Neuron neuron2 = new SimpleNeuron("21");
+		connection.setNextNeuron(neuron2);
+		assertTrue(connection.getNextNeuron().getId().equals("21"));
+		connection.show();
+	}
+
+	
+	@Test
+	public void testGetId() {
+		Connection connection1 = new Connection(null, 8.7);
+		assertTrue(connection1.getId().equals("NA"));
+
+		Neuron neuron = new SimpleNeuron("9");
+		Connection connection2 = new Connection(neuron, 8.7);
+		assertTrue(connection2.getId().equals("9"));
+		
+	}
+	 
+
+	@Test
+	public void testGetWeight() {
+		Neuron neuron = new SimpleNeuron("16");
+		Connection connection = new Connection(neuron, 12.4);
+		assertTrue(connection.getWeight()==12.4);
 	}
 	
-	@Test(expected=AmoreJException.class)
+
+	@Test
+	public void testSetWeight() {
+		Neuron neuron = new SimpleNeuron("16");
+		Connection connection = new Connection(neuron, 12.4);
+	    connection.setWeight(2.2);
+		assertTrue(connection.getWeight()==2.2);
+	}
+	
+
+	@Test
+	public void testShow() {
+		Neuron neuron = new SimpleNeuron("16");
+		Connection connection = new Connection(neuron, 12.4);
+		connection.show();
+	}
+
+	@Test(expected = AmoreJException.class)
+		public void testValidateWeight() throws AmoreJException {
+		Neuron neuron = new SimpleNeuron("16");
+		Connection connection = new Connection(neuron, 12.4/0.0 );
+		connection.validate();
+	}
+	
+	
+
+	@Test(expected = AmoreJException.class)
+	public void testValidateFromEmptyId() throws AmoreJException {
+		Neuron neuron = new SimpleNeuron("");
+		Connection connection = new Connection(neuron, 2.7 );
+		connection.validate();
+}
+	 
+
+	@Test(expected = AmoreJException.class)
+	public void testValidateFromNullId() throws AmoreJException {
+		Connection connection = new Connection(null, 2.7 );
+		connection.validate();
+}
+
+	
+	@Test(expected = AmoreJException.class)
 	public void testException() throws AmoreJException {
 		// llamada a la situacion que genera la excepcion
 		throw new AmoreJException("Great!");
